39a7164efed87b25461d2060c4b7f98a900d8e2a
Michael Borromeo <mbborromeo@gmail.com>
2017-12-19 22:14:39 -0800

    Add files via upload

---
+++ tiles.js
@@ -0,0 +1,75 @@
+( function(){ //IIFE means this will immediately invoke
+    console.log("IIFE");
+    
+    var $tilePlaceholder = $("#tile-placeholder");
+    var $tiles = $("#tile-placeholder .tile"); //store all tiles
+    var $buttons = $("#tile-buttons"); //buttons div
+    var tagged = {}; //plain JS object/array
+    
+    $tiles.each( function(){
+        var currentTile = this; //reference to .tile div element
+        console.log("currentTile is: " + currentTile );
+        var tags = $( this ).data("tags"); //get the elements data-tags String value using jQuery data()
+        console.log( "tags is: " + tags );
+
+        if( tags ){ //if elements actually has tags value
+            /*
+            var tagsSplittedIntoArray = tags.split(","); //plain JS split()
+            console.log( "tagsSplittedIntoArray is: " + tagsSplittedIntoArray );
+            tagsSplittedIntoArray.forEach( function(t){
+                if( tagged[t] == null ){
+                    tagged[t] = [];
+                }
+                tagged[t].push( currentTile );
+            } );
+            */
+            
+            //use .trim() to remove whitespace from beginning and end of string
+            tags.split(",").forEach( function(t){
+                console.log("tags split forEach tagged[t] is " + tagged[t] );
+                var tTrimmed = t.trim();
+                if( tagged[tTrimmed] == null ){
+                    tagged[tTrimmed] = [];
+                }
+                tagged[tTrimmed].push( currentTile );
+            } );
+        }
+    } );
+    
+    
+    //if you want to order tags in tagged object alphabetically, may have to use array instead of object
+    //as tagged.sort() does not work...
+    
+    
+    //console.log( "Count of tagged['Responsive Front End Web Development'] is: " + tagged["Responsive Front End Web Development"].length );
+    //$buttons.append( tagged["Web Design"] );
+    
+    /* Create buttons */
+    //buttons 'Show All' code
+    $('<button />', {
+        text: 'Show All',
+        class: 'active',
+        click: function(){
+            console.log("All button was clicked");
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+            $tiles.show();            
+        }
+    }).appendTo( $buttons );
+    
+    //iterate over each entry in the array 'tagged' to create the rest of the buttons
+    $.each( tagged, function(tagName){
+            $(  "<button />", {
+                text: tagName + ' (' + tagged[tagName].length + ')',
+                click: function(){
+                    console.log("other button was clicked");
+                    $(this).addClass('active');
+                    $(this).siblings().removeClass('active');
+                    
+                    //for each .tile in $tiles, hide it, then apply filter check, then show it
+                    $tiles.hide().filter( tagged[tagName] ).show(); 
+                }//end click
+            }).appendTo( $buttons );      
+    });//end anon function/end each
+    
+}() );
